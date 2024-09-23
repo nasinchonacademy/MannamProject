88d90f2ef932eaa53252bdac0dd457a3ca269062
nasil <sktlf113@naver.com>
2024-09-23 21:19:36 +0900

    Initial commit

---
+++ src/main/java/org/zerock/wantuproject/WantUprojectApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.wantuproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantUprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WantUprojectApplication.class, args);
+    }
+
+}
